0d9b6cdbabdd8b7deda1d5fd353cea34f40c170d
Michael Crawford <michael@michaels-mbp.lan>
2018-05-22 09:19:42 -0400

    added fizzbuzz template

---
+++ week1/fizzbuzz.js
@@ -0,0 +1,23 @@
+// use this template for your fizzbuzz code
+
+function fizzbuzz(numbers) {
+  // assume numbers is an array of integers.  fizzbuzz should return a new
+  // array where multiples of 3s and 5s become "Fizz" "Buzz" or "FizzBuzz"
+  // using numbers.map()
+  // YOUR CODE HERE
+}
+
+// here's a test case to see if you got it right
+// DON'T CHANGE THIS
+function test() {
+  let arr = [];
+  for (let i = 1; i < 101; i++) {
+    arr.push(i);
+  }
+  let updatedArray = fizzbuzz(arr);
+  updatedArray.array.forEach(element => {
+    console.log(element);
+  });
+}
+
+test();
