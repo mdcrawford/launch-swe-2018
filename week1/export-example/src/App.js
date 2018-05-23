bbe8643cb1e3e700dd36e961866a0d7d49903d91
Michael Crawford <michael@michaels-mbp.lan>
2018-05-23 10:22:08 -0400

    export syntax example

---
+++ week1/export-example/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Child from "./Child.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Child />
+      </div>
+    );
+  }
+}
+
+export default App;
